89978bc29f9b904507582ffb4bca391963facd6a
gilles zwijsen <gilles.zwijsen@student.kdg.be>
2024-06-24 20:51:27 +0200

    remove reward token

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -5,10 +5,8 @@ pragma solidity >=0.7.0 <0.9.0;
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {StakingToken} from "contracts/tokens/StakingToken.sol";
-import {RewardToken} from "contracts/tokens/RewardToken.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract Staking is Initializable {
     using SafeERC20 for IERC20;
@@ -18,14 +16,13 @@ contract Staking is Initializable {
     address public owner;
 
     StakingToken public stakingToken;
-    RewardToken public rewardToken;
     uint256 public rewardRate;
+    address[] public stakers;
 
     constructor() {
         _disableInitializers();
         owner = msg.sender;
         stakingToken = new StakingToken(1000);
-        rewardToken = new RewardToken(1000);
         rewardRate = 1;
     }
 
@@ -41,19 +38,18 @@ contract Staking is Initializable {
         locked = false;
     }
 
+    mapping(address => Stake) public stakes;
     mapping(address => uint256) public balances;
 
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
     event RewardPaid(address indexed user, uint256 reward);
 
-    //0x0000000000000000000000000000000000000000
     // Deposit ETH or ERC20 tokens
-    function deposit(address token, uint256 amount)
-        external
-        payable
-        noReentrant
-    {
+    function deposit(
+        address token,
+        uint256 amount
+    ) external payable noReentrant {
         require(amount > 0, "Amount must be greater than 0");
 
         if (token == address(0)) {
@@ -66,6 +62,10 @@ contract Staking is Initializable {
             IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
             balances[msg.sender] = balances[msg.sender].add(amount);
             stakingToken.mint(address(this), amount);
+        }
+
+        if (stakes[msg.sender].amount == 0) {
+            stakers.push(msg.sender);
         }
 
         emit Staked(msg.sender, amount);
@@ -88,6 +88,11 @@ contract Staking is Initializable {
         // Update user's balance
         balances[msg.sender] = balances[msg.sender].sub(amount);
 
+        // Remove from stakers array if withdrawing all
+        if (balances[msg.sender] == 0) {
+            _removeFromStakers(msg.sender);
+        }
+
         // Convert burned tokens to ETH (example: 1 staking token = 1 ETH)
         uint256 ethReceived = amount; // Adjust this based on your conversion rate or mechanism
 
@@ -97,43 +102,52 @@ contract Staking is Initializable {
         emit Withdrawn(msg.sender, amount);
     }
 
-    function stake(uint256 amount) external noReentrant {
-        require(
-            amount <= stakingToken.balanceOf(msg.sender),
-            "Not enough STATE tokens"
-        );
+    function reward() external payable noReentrant {
+        uint256 totalBalance = stakingToken.balanceOf(address(this));
+        require(totalBalance > 0, "No staking tokens to distribute rewards");
+
+        uint256 ethReceived = msg.value;
+
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address staker = stakers[i];
+            uint256 stakerBalance = balances[staker];
+            if (stakerBalance > 0) {
+                uint256 ethToSend = ethReceived.mul(stakerBalance).div(
+                    totalBalance
+                );
+                payable(staker).transfer(ethToSend);
+            }
+        }
     }
 
-    function reward() external noReentrant {
-        emit RewardPaid(msg.sender, 100);
-    }
-
-    function balanceOfStakingToken(address account)
-        external
-        view
-        returns (uint256)
-    {
+    function balanceOfStakingToken(
+        address account
+    ) external view returns (uint256) {
         return stakingToken.balanceOf(account);
-    }
-
-    function balanceOfRewardToken(address account)
-        external
-        view
-        returns (uint256)
-    {
-        return rewardToken.balanceOf(account);
     }
 
     function totalStakingTokens() external view returns (uint256) {
         return stakingToken.balanceOf(address(this));
     }
 
-    function getBalancePlayer(address _playerAddress) external view returns (uint256) {
+    function getBalancePlayer(
+        address _playerAddress
+    ) external view returns (uint256) {
         return balances[_playerAddress];
+    }
+
+    function _removeFromStakers(address _staker) internal {
+        for (uint256 i = 0; i < stakers.length; i++) {
+            if (stakers[i] == _staker) {
+                stakers[i] = stakers[stakers.length - 1];
+                stakers.pop();
+                break;
+            }
+        }
     }
 }
 
-interface IWETH is IERC20, IERC20Metadata {
+interface IWETH is IERC20 {
     function deposit() external payable;
 
     function withdraw(uint256 amount) external;
--- test/staking/staking.challenge.js
+++ test/staking/staking.challenge.js
@@ -1,33 +1,107 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
-describe('Init Staking', function () {
-    let deployer, player, someUser;
+describe('Staking Contract', function () {
+    let deployer, player1, player2, player3, player4;
     let stakingContract;
 
-
     before(async function () {
-        [deployer, player, someUser] = await ethers.getSigners();
-        stakingContract = await (await ethers.getContractFactory('Staking', deployer)).deploy();
+        [deployer, player1, player2, player3, player4] = await ethers.getSigners();
+        const Staking = await ethers.getContractFactory('Staking', deployer);
+        stakingContract = await Staking.deploy();
+        await stakingContract.deployed();
     });
 
     it("Should have deposited correctly", async function () {
-        const amount = ethers.utils.parseEther("1"); //1eth
-        await stakingContract.connect(player).deposit("0x0000000000000000000000000000000000000000", amount, {
+        const amount = ethers.utils.parseEther("1"); // 1 ETH
+        await stakingContract.connect(player1).deposit("0x0000000000000000000000000000000000000000", amount, {
             value: amount
         });
 
-        const balance = await stakingContract.getBalancePlayer(player.address);
+        const balance = await stakingContract.getBalancePlayer(player1.address);
         expect(balance).to.equal(amount, "Player should have staked the correct amount");
     });
 
-
     it("Should have withdrawn correctly", async function () {
-        const amount = ethers.utils.parseEther("1"); //1eth
-        await stakingContract.connect(player).withdraw(amount);
+        const amount = ethers.utils.parseEther("1"); // 1 ETH
+        await stakingContract.connect(player1).withdraw(amount);
 
-        const balance = await stakingContract.getBalancePlayer(player.address);
-        expect(balance).to.equal(0, "Player should have staked the correct amount");
+        const balance = await stakingContract.getBalancePlayer(player1.address);
+        expect(balance).to.equal(0, "Player should have withdrawn the correct amount");
+    });
+
+    it("Should distribute rewards correctly in ETH", async function () {
+        // Make sure player has some staked ETH
+        const initialStakeAmount = ethers.utils.parseEther("2"); // 2 ETH
+        await stakingContract.connect(player1).deposit("0x0000000000000000000000000000000000000000", initialStakeAmount, {
+            value: initialStakeAmount
+        });
+
+        // Capture initial ETH balance of player
+        const initialEthBalance = await ethers.provider.getBalance(player1.address);
+
+        // Call reward function with some ETH
+        const rewardAmount = ethers.utils.parseEther("1"); // 1 ETH
+
+        // Perform the reward transaction
+        const tx = await stakingContract.reward({ value: rewardAmount });
+        await tx.wait();
+
+        // Check ETH balance of player after reward distribution
+        const finalEthBalance = await ethers.provider.getBalance(player1.address);
+
+        // Assert that player's ETH balance has increased within an acceptable margin
+        const expectedBalanceIncrease = ethers.BigNumber.from("999999999999999500");
+        expect(finalEthBalance.sub(initialEthBalance)).to.be.closeTo(expectedBalanceIncrease, ethers.BigNumber.from("10000"), "Player should receive correct ETH reward");
+    });
+
+
+    it("Should distribute rewards correctly among stakers", async function () {
+        // Deposit initial amounts for players
+        const amountPlayer1 = ethers.utils.parseEther("1");   // 1 ETH
+        const amountPlayer2 = ethers.utils.parseEther("2");   // 2 ETH
+        const amountPlayer3 = ethers.utils.parseEther("1.5"); // 1.5 ETH
+        const amountPlayer4 = ethers.utils.parseEther("0.5"); // 0.5 ETH
+
+
+              // Capture initial ETH balances of players
+              const initialEthBalancePlayer1 = await ethers.provider.getBalance(player1.address);
+              const initialEthBalancePlayer2 = await ethers.provider.getBalance(player2.address);
+              const initialEthBalancePlayer3 = await ethers.provider.getBalance(player3.address);
+              const initialEthBalancePlayer4 = await ethers.provider.getBalance(player4.address);
+      
+
+        await stakingContract.connect(player1).deposit("0x0000000000000000000000000000000000000000", amountPlayer1, { value: amountPlayer1 });
+        await stakingContract.connect(player2).deposit("0x0000000000000000000000000000000000000000", amountPlayer2, { value: amountPlayer2 });
+        await stakingContract.connect(player3).deposit("0x0000000000000000000000000000000000000000", amountPlayer3, { value: amountPlayer3 });
+        await stakingContract.connect(player4).deposit("0x0000000000000000000000000000000000000000", amountPlayer4, { value: amountPlayer4 });
+
+  
+        // Call reward function with some ETH
+        const rewardAmount = ethers.utils.parseEther("5"); // 5 ETH
+
+        // Perform the reward transaction
+        const tx = await stakingContract.reward({ value: rewardAmount });
+        await tx.wait();
+
+        // Check ETH balance of players after reward distribution
+        const finalEthBalancePlayer1 = await ethers.provider.getBalance(player1.address);
+        const finalEthBalancePlayer2 = await ethers.provider.getBalance(player2.address);
+        const finalEthBalancePlayer3 = await ethers.provider.getBalance(player3.address);
+        const finalEthBalancePlayer4 = await ethers.provider.getBalance(player4.address);
+
+        // Calculate expected rewards based on stakes
+        const totalStaked = amountPlayer1.add(amountPlayer2).add(amountPlayer3).add(amountPlayer4);
+        const rewardForPlayer1 = rewardAmount.mul(amountPlayer1).div(totalStaked);
+        const rewardForPlayer2 = rewardAmount.mul(amountPlayer2).div(totalStaked);
+        const rewardForPlayer3 = rewardAmount.mul(amountPlayer3).div(totalStaked);
+        const rewardForPlayer4 = rewardAmount.mul(amountPlayer4).div(totalStaked);
+
+        // Assert that players' ETH balances have increased within an acceptable margin
+        expect(finalEthBalancePlayer1).to.be.gt(initialEthBalancePlayer1, "Player 1 should gain more ETH than they initially had");
+        expect(finalEthBalancePlayer2).to.be.gt(initialEthBalancePlayer2, "Player 2 should gain more ETH than they initially had");
+        expect(finalEthBalancePlayer3).to.be.gt(initialEthBalancePlayer3, "Player 3 should gain more ETH than they initially had");
+        expect(finalEthBalancePlayer4).to.be.gt(initialEthBalancePlayer4, "Player 4 should gain more ETH than they initially had");
     });
 
 });
